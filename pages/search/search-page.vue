c91bfd0a1a03a70351fcdde63eb7623b3a3a77fa
YUHSIENH <70119592+YUHSIENH@users.noreply.github.com>
2021-10-14 22:04:43 +0800

    structure

--- pages/search/search-page.vue
+++ pages/search/search-page.vue
@@ -1,8 +1,19 @@
 <template>
-	<p>This is search page</p>
+	<h1>{{word}}</h1>
+	<input type="button" value="🗣US" /><input type="button" value="🗣UK" />
+        <p>{{pos}}</p>
+        <h4>{{meaning}}</h4>
+	<p>{{examples}}</p>
 </template>
 
-<script>
+<script type="text/javascript">
+
+    var data = { word: "formar",kk:"/FcMeR/", pos: "adj.", meaning: "以前的", examples:"・his former wife..."}
+    var vm = new Vue({
+        el: '#vue_det',
+        data: data
+    })
+   
 </script>
 
 <style>
