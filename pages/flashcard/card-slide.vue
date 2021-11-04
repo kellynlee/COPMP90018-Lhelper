a29730d09e5abcf171f8224400348a0483bcec21
zhengyuegao <zhengyuegao@tencent.com>
2021-11-05 01:01:51 +0800

    big change to flashcard

---
+++ pages/flashcard/card-slide.vue
@@ -0,0 +1,269 @@
+<template>
+	<div class="tantan">
+
+		<movable-area>
+			<view class="card-footer">
+				<u-button @click="scrolldown">Check Detail</u-button>
+			</view>
+			<view style="text-align: center;">
+				<h3>{{size-index}}/{{size}}</h3>
+			</view>
+			<view class="card-button-group">
+				<u-button type="error" class="inside-u-button" plain @click="forget">Forget</u-button>
+				<u-button type="success" class="inside-u-button" plain @click="remember">Remember</u-button>
+			</view>
+			<block v-for="(item, index) in goods" :key="item.id">
+				<movable-view :damping="20" :x="item.x" :y="item.y" inertia out-of-bounds direction="all"
+					@change="onMovableChange($event, index)">
+					<div class="item" :style="{
+              position: 'relative',
+              zIndex: index + 1,
+              transform: `
+                scale(${
+                  index == goods.length - 1
+                    ? 1
+                    : 1 - (goods.length - index) / 100
+                })
+                translateY(${-index * 10}px)`,
+              transition: 'all .3s',
+            }">
+			
+						<!-- <u-image :src="item.cover" :width="400" :height="400"></u-image> -->
+						<view class="flashcard-item" width="100%" height="100%">
+							<h1>{{item.word}}</h1>
+							<p>test</p>
+						</view>
+					</div>
+				</movable-view>
+				<view>
+					<p>test</p>
+				</view>
+			</block>
+		</movable-area>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['wordList'],
+		data() {
+			return {
+				index: 0,
+				size: 0,
+				word: '',
+				goods: [],
+				allWordList: [],
+				pageNum: 1,
+			};
+		},
+
+		created() {},
+		watch: {
+			wordList() {
+				for (let word of this.wordList) {
+					this.allWordList.push(word)
+				}
+				this.size = this.wordList.length
+				this.getGoodList(3);
+				this.word = this.goods[this.goods.length - 1].word
+			}
+		},
+		mounted() {
+			// this.getGoodList(3);			
+		},
+		methods: {
+			/**
+			 * 模拟获取数据
+			 */
+			scrolldown() {
+				this.$emit('scrolldown')
+			},
+			getGoodList(pageSize = 1) {
+				console.log("获取新数据");
+				for (let index = 0; index < pageSize; index++) {
+					if (this.allWordList.length <= 0) {
+						return
+					}
+					let word = this.allWordList.shift()
+					console.log(word)
+					this.goods.unshift({
+						id: String(index) + String(this.pageNum),
+						...word,
+						x: 0,
+						y: 0,
+
+						old: {
+							x: 0,
+							y: 0,
+						},
+					});
+
+					this.pageNum += 1;
+				}
+			},
+
+			/**
+			 * 移动回调
+			 */
+			onMovableChange(event, index) {
+				let item = this.goods[index];
+				let {
+					x,
+					y,
+					source
+				} = event.detail; // 赋值当前坐标
+				// console.log(x, y, source);
+
+				item.old.x = x;
+				item.old.y = y;
+
+				// 移动结束/手指离开屏幕
+				if (source == "friction") this.onMovableEnd(index);
+			},
+
+			/**
+			 * 移动结束回调
+			 */
+			onMovableEnd(index) {
+				let item = this.goods[index];
+				let {
+					x,
+					y
+				} = item.old; // 获取当前坐标
+
+				if (x >= 20) {
+					// 超出右边界
+					this.removeItem(index, "like");
+				} else if (x <= -20) {
+					// 超出左边界
+					this.removeItem(index, "unlike");
+				} else {
+					// 没有超出边界，回到中心点
+					this.resetItem(index);
+				}
+			},
+
+			/**
+			 * 移除卡片
+			 */
+			removeItem(index, mode = "like") {
+				this.index++;
+				let item = this.goods[index];
+				item.x = item.old.x;
+				item.y = item.old.y;
+
+				// 滚动到屏幕外
+				this.$nextTick(() => {
+					item.x = mode == "like" ? 1000 : -1000; // 喜欢向右滚出、不喜欢向左滚出
+					item.y = item.old.y <= 0 ? item.old.y - 100 : item.old.y + 100; // y小于0说明向上滑出，加一点向上的偏移值，效果好一些
+
+					setTimeout(() => {
+						this.goods.splice(index, 1); // 移除数据（等动画结束）
+						this.getGoodList(); // 继续请求新的数s据
+						index++;
+						if (this.goods.length === 0) {
+							this.$emit('setFinished', true)
+						}
+						this.word = this.goods[this.goods.length - 1].word
+						this.$emit('setword', this.word);
+					}, 200);
+				});
+			},
+			forget() {
+				this.removeItem(this.goods.length - 1, "unlike");
+				this.$emit('setForget', this.index);
+			},
+			remember() {
+				this.removeItem(this.goods.length - 1, "like");
+				this.$emit('setRemember', this.index);
+			},
+			/**
+			 * 重设卡片
+			 */
+			resetItem(index) {
+				let item = this.goods[index];
+				item.x = item.old.x;
+				item.y = item.old.y;
+
+				this.$nextTick(() => {
+					item.x = 0;
+					item.y = 0;
+				});
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.tantan {
+		height: 100%;
+		overflow: hidden;
+		// background-color: #c7e5c7;
+
+		movable-area {
+			position: relative;
+			width: 300vw;
+			height: 100%;
+			left: calc(-100vw);
+			top: calc(0vh);
+		}
+
+		movable-view {
+			position: absolute;
+			top: 0;
+			right: 0;
+			bottom: 0;
+			left: 0;
+			margin-top: 150rpx;
+			margin-left: auto;
+			margin-right: auto;
+			// margin: auto;
+			width: 500rpx;
+			height: 500rpx;
+		}
+
+		.item {
+			border-radius: 30rpx;
+			box-shadow: 0 0 10rpx 2rpx rgba($color: black, $alpha: 0.2);
+			overflow: hidden;
+		}
+
+		uni-page-body {
+			height: 100%;
+		}
+
+		.page-body {
+			height: 100%;
+		}
+
+		.flashcard-item {
+			width: 500rpx;
+			height: 500rpx;
+			background-color: #FFFFFF;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			flex-direction: column;
+		}
+
+		.card-button-group {
+			position: absolute;
+			margin-top: 700rpx;
+			width: 100vw;
+			margin-left: 100vw;
+			display: flex;
+			justify-content: center;
+
+			.inside-u-button {
+				width: 200rpx;
+			}
+		}
+
+		.card-footer {
+			position: absolute;
+			bottom: 0px;
+			text-align: center;
+			width: 100%;
+		}
+	}
+</style>
--- pages/flashcard/flashcard-page.vue
+++ pages/flashcard/flashcard-page.vue
@@ -1,253 +1,172 @@
 <template>
-  <view class="page-body scroll-disable">
-    <!-- <view class="page-body body-flex scroll-disable" v-show="ShowCard" :animation="animationData">
-			<view>
-				<view class="words-number"><h2>{{count}}/{{wordList.length}}</h2> </view>
-				<view class="flashcard-wrap">
-					<view class="side-arrow" v-show="!noTask"><p><</p></view>
-					<cardSlide v-if="Show&&wordList.length!==0" ref="flashcard" :wordList='wordList' @OnReloadForget="reloadForget" @OnRemember='rememberEvent' @OnForget='forgetEvent' @error='errEvent'></cardSlide>
-					<view class="side-arrow" v-show="!noTask"><p>></p></view>
+	<view  class="page-body scroll-disable">
+		<view class="page-body scroll-disable">
+			<view class="page-body body-flex scroll-disable" :animation="animationData">
+				<view v-show="isFinished">
+					<view class="no-data">
+						<u-image mode="aspectFit" height="200px" src="../../static/icons/no-data.svg"
+							bg-color="#ffffff">
+						</u-image>
+						<text class="text">Congrats! You don't have words to review today</text>
+					</view>
 				</view>
-				<view class="button-group"  v-show="!noTask">
-					<u-button plain @click="forget" type="error" class="button-style">forget</u-button>
-					<u-button plain @click="remember" type="success"  class="button-style">remember</u-button>
-				</view>
-			</view>
-			<view  v-show="!noTask&&ShowCard" class="footer click-btn" @click="scrollDown">
-				<u-icon name="arrow-down"></u-icon>
-				<text>Click to see the meaning</text>
+				<cardslide @setword='setword' @setForget='forgetEvent' @setRemember='rememberEvent' @scrolldown='scrolldown' v-show="!isFinished" :wordList='wordList' @setFinished='setFinished'></cardslide>
 			</view>
 		</view>
-		<view  v-show="ShowCard" class="page-body scroll-enable" :animation="animationData">
+		<view class="page-body scroll-enable" :animation="animationData">
 			<view @click="scrollUp" class="click-btn">
 				<u-icon name="arrow-up"></u-icon>
 				<text>Click to return</text>
 			</view>
-			
-			<dictionary ref="dictionary" v-if="word!==''" :isAdd="true" :word="word" ></dictionary>
+			<dictionary ref="dictionary" v-if="word!==''" :isAdd="true" :word="word"></dictionary>
 		</view>
-		<view  v-show="!ShowCard" ><view class="no-data">
-        <u-image
-          mode="aspectFit"
-          height="200px"
-          src="../../static/icons/no-data.svg"
-          bg-color="#ffffff"
-        ></u-image>
-        <text class="text"
-          >Congrats! You don't have words to review today</text
-        >
-      </view> </view> -->
-  </view>
+	</view>
 </template>
-
 <script>
-// import dictionary from "../../components/dictionary.vue"
-// import cardSlide from './vue-card-slide.vue'
-// import {
-// 	FLASHCARD_URL
-// } from "../../utils/paths.js";
-// import {
-// 	getUrl,
-// 	getArray
-// } from "../../utils/methods.js"
-// const axios = require("axios");
-// export default {
-// 	components:{cardSlide,dictionary},
-// 	data(){
-// 		return {
-// 			word:'',
-// 			ShowCard:true,
-// 			animationData:{},
-// 			count:0,
-// 			Show:true,
-// 			wordList:[
-// 			],
-// 			forgetWords:[]
-// 		}
-// 	},
-// 	onUnload() {
-// 		this.animationData = {}
-// 		// 页面关闭后清空数据
-// 	},
-// 	onload(option) {
-// 		console.log(option)
-// 		this.animation = uni.createAnimation()
-// 		console.log("test")
-// 		// 创建动画实例
-// 	},
-// 	onShow() {
-// 		var animation = uni.createAnimation({
-// 			duration: 300,
-// 			timingFunction: 'ease-out',
-// 		})
-// 		this.animation = animation
+	import dateFormat from "dateformat";
+	import cardslide from './card-slide.vue'
+	import dictionary from "../../components/dictionary.vue"
+	import {
+		FLASHCARD_URL
+	} from "../../utils/paths.js";
+	import {
+		getArray,
+		request
+	} from "../../utils/methods.js";
+	export default {
+		onShow() {
+			var animation = uni.createAnimation({
+				duration: 300,
+				timingFunction: 'ease-out',
+			})
+			this.animation = animation
+		
+		},
+		created() {
+			let date = this.$store.state.flashcard_date;
+			this.$u.vuex("flashcard_date", "");
+			console.log(date);
+			if (this.wordList.length == 0 && date !== "") {
+				let url =
+					FLASHCARD_URL + "/" + this.$store.state.vuex_user.id + "/" + date;
+				let option = {
+					url: url,
+				};
+				request(option).then((res) => {
+					console.log(res.data);
+					if (res.data) {
+						let obj = res.data;
+						console.log(obj[Object.keys(obj)[0]]);
+						this.wordList = getArray(obj[Object.keys(obj)[0]].word_list);
+						console.log(this.wordList);
+						this.count = this.wordList.length;
+						this.word = this.wordList[0].word;
+					} else {
+						// this.ShowCard = false;
+					}
+				});
+			} else {
+				// this.ShowCard = false;
+			}
+		},
+		onUnload() {
+			this.animationData = {}
+			// 页面关闭后清空数据
+		},
+		onLoad(option) {
+			console.log(option);
+			const now = new Date().getTime();
+			const today = dateFormat(now, "yyyy-mm-dd");
+			if (!option.date) {
+				this.$u.vuex("flashcard_date", today);
+			} else {
+				this.$u.vuex("flashcard_date", option.date);
+			}
+			this.animation = uni.createAnimation();
+			console.log("test");
+			// 创建动画实例
+		},
+		components: {
+			cardslide,
+			dictionary
+		},
 
-// 	},
-// 	created(){
-// 		let date = this.$store.state.flashcard_date;
-// 		this.$u.vuex('flashcard_date', '')
-// 		console.log(date)
-// 		if(this.wordList.length==0&&date!==''){
-// 			axios.get(getUrl(FLASHCARD_URL)).then((res) => {
-// 				let obj = res.data[this.$store.state.vuex_user.id][date]
-// 				console.log(obj[Object.keys(obj)[0]])
-// 				this.wordList = getArray(obj[Object.keys(obj)[0]].word_list)
-// 				console.log(this.wordList)
-// 				this.count = this.wordList.length;
-// 				this.word = this.wordList[0].word;
-// 			})
-// 		}else{
-// 			this.ShowCard = false;
-// 		}
-// 	},
-// 	computed:{
-// 		noTask(){
-// 			return this.count===0
-// 		}
-// 	},
-// 	methods:{
-// 		scrollDown:function(){
-// 			console.log(this.animation)
-// 			this.animation.translateY('-100%').step()
-// 			// 导出动画数据传递给data层
-// 			this.animationData = this.animation.export()
-// 		},
-// 		scrollUp(){
-// 			console.log(this.animation)
-// 			this.animation.translateY('0%').step()
-// 			// 导出动画数据传递给data层
-// 			this.animationData = this.animation.export()
-// 		},
-// 		forget:function(){
-// 			this.$refs.flashcard.success();
-// 		},
-// 		remember:function(){
-// 			this.$refs.flashcard.error();
-// 		},
-// 		checkFinish(index){
-// 			console.log(index,this.wordList.length)
-// 			if(index>=this.wordList.length){
-// 				this.noTask = false;
-// 			}
-// 		},
-// 		rememberEvent:function(word, index){
-// 			// this.word = ''
-// 			if(index<this.wordList.length-1){
-// 				this.word = this.wordList[index+1].word
-// 			}
-// 			// this.$refs.dictionary.translate()
-// 			for(let w of this.wordList){
-// 				if(word.word=== w.word && !w.remember){
-// 					console.log(w)
-// 					w['remember'] = true
-// 					this.count--;
-
-// 				}
-// 			}
-// 			// this.checkFinish(index)
-// 		},
-// 		forgetEvent:function(word, index){
-// 			// this.word = ''
-// 			if(index<this.wordList.length-1){
-// 				this.word = this.wordList[index+1].word
-// 			}
-// 			// this.$refs.dictionary.translate()
-// 			for(let w of this.wordList){
-// 				if(word.word=== w.word){
-// 					w['remember'] = false
-// 					this.count--;
-// 				}
-// 			}
-// 			this.forgetWords.push(word)
-// 			console.log(word)
-// 			// this.checkFinish(index)
-// 		},
-// 		async reloadForget(){
-// 			this.$u.vuex('flash_words', this.wordList)
-// 			this.$u.route("/pages/flashcard/Summary");
-// 			// this.wordList = this.forgetWords;
-// 			// this.forgetWords = []
-// 			// this.Show= false
-// 			// // 建议加上 nextTick 微任务
-// 			// // 否则在同一事件内同时将tableShow设置false和true有可能导致组件渲染失败
-// 			// await this.$nextTick(function(){
-// 			// 	// 加载
-// 			// 	this.Show= true
-// 			// })
-// 		}
-
-// 	}
-
-// };
+		data() {
+			return {
+				word:'test',
+				animationData: {},
+				isFinished: false,
+				wordList: [],
+			}
+		},
+		methods: {
+			forgetEvent: function(index) {
+				// this.word = ''
+				if (index < this.wordList.length - 1) {
+					this.word = this.wordList[index + 1].word
+				}
+				// this.$refs.dictionary.translate()
+				for (let w of this.wordList) {
+					if (word.word === w.word) {
+						w['remember'] = false
+						this.count--;
+					}
+				}
+				console.log(word)
+				// this.checkFinish(index)
+			},
+			setword(word){
+				this.word = word;
+			},
+			rememberEvent: function(index) {
+				if (index < this.wordList.length - 1) {
+					this.word = this.wordList[index + 1].word
+				}
+				// this.$refs.dictionary.translate()
+				for (let w of this.wordList) {
+					if (word.word === w.word && !w.remember) {
+						console.log(w)
+						w['remember'] = true
+						this.count--;
+			
+					}
+				}
+				// this.checkFinish(index)
+			},
+			setFinished(status) {
+				this.isFinished = status
+			},
+			scrollUp() {
+				console.log(this.animation)
+				this.animation.translateY('0%').step()
+				// 导出动画数据传递给data层
+				this.animationData = this.animation.export()
+			},
+			scrolldown(){
+				console.log("tasdasjdnjkas")
+				this.animation.translateY('-100%').step()
+				// 导出动画数据传递给data层
+				this.animationData = this.animation.export()
+			},
+		}
+	}
 </script>
 
-<style scoped>
-.page-body {
-  height: 100%;
-}
-.flashcard-wrap {
-  padding-top: 40px;
-  display: flex;
-  align-content: center;
-  flex-direction: row;
-  justify-content: space-evenly;
-}
-.side-arrow {
-  display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
-  align-content: center;
-  justify-content: center;
-}
-.button-group {
-  margin-top: 100px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-}
-.button-style {
-  width: 200rpx;
-}
-.words-number {
-  padding-top: 30px;
-  text-align: center;
-}
-.scroll-disable {
-  overflow: hidden;
-}
-.scroll-enable {
-  overflow: scroll;
-}
-uni-page-body {
-  height: 100%;
-}
-.footer {
-  bottom: 0px;
-  align-self: center;
-}
-.body-flex {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-.click-btn {
-  text-align: center;
-}
-.no-data {
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  margin: 1rem;
-  padding-top: 1rem;
-  border-radius: 1rem;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
-    rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
-  .text {
-    font-size: 1rem;
-    font-weight: bold;
-    display: block;
-    margin: 1rem;
-    margin-left: 20px;
-  }
-}
+<style>
+
+	uni-page-body {
+		width: 100vh;
+		height: 100vh;
+	}
+
+	.page-body {
+		height: 100vh;
+	}
+	.scroll-disable {
+		overflow: hidden;
+	}
+	
+	.scroll-enable {
+		overflow: scroll;
+	}
 </style>
