6f1da93b08d382b7aa7f00bb3bbc2a46f59282a0
kellyn.li <kellynlee0011@outlook.com>
2021-11-04 13:59:16 +0800

    fixing issues and adding sound for actions

--- pages/flashcard/flashcard-page.vue
+++ pages/flashcard/flashcard-page.vue
@@ -13,10 +13,17 @@
 					<u-button plain @click="remember" type="success"  class="button-style">remember</u-button>
 				</view>
 			</view>
-			<view  v-show="!noTask&&ShowCard" class="footer" @click="scrollDown"><p>scrollDown</p></view>
+			<view  v-show="!noTask&&ShowCard" class="footer click-btn" @click="scrollDown">
+				<u-icon name="arrow-down"></u-icon>
+				<text>Click to see the meaning</text>
+			</view>
 		</view>
 		<view  v-show="ShowCard" class="page-body scroll-enable" :animation="animationData">
-			<p @click="scrollUp">scrollUp</p>
+			<view @click="scrollUp" class="click-btn">
+				<u-icon name="arrow-up"></u-icon>
+				<text>Click to return</text>
+			</view>
+			
 			<dictionary ref="dictionary" v-if="word!==''" :isAdd="true" :word="word" ></dictionary>
 		</view>
 		<view  v-show="!ShowCard" ><view class="no-data">
@@ -36,6 +43,7 @@
 <script>
 	import dictionary from "../../components/dictionary.vue"
 	import cardSlide from './vue-card-slide.vue'
+	import dateFormat from "dateformat";
 	import {
 		FLASHCARD_URL
 	} from "../../utils/paths.js";
@@ -62,8 +70,15 @@
 			this.animationData = {}
 			// 页面关闭后清空数据
 		},
-		onload(option) {
+		onLoad(option) {
 			console.log(option)
+			const now = new Date().getTime();
+			const today = dateFormat(now, "yyyy-mm-dd");
+			if (!option.date) {
+				this.$u.vuex('flashcard_date', today)
+			} else {
+				this.$u.vuex('flashcard_date', option.date)
+			}
 			this.animation = uni.createAnimation()
 			console.log("test")
 			// 创建动画实例
@@ -81,13 +96,19 @@
 			this.$u.vuex('flashcard_date', '')
 			console.log(date)
 			if(this.wordList.length==0&&date!==''){
-				axios.get(getUrl(FLASHCARD_URL)).then((res) => {
-					let obj = res.data[this.$store.state.vuex_user.id][date]
-					console.log(obj[Object.keys(obj)[0]])
-					this.wordList = getArray(obj[Object.keys(obj)[0]].word_list)
-					console.log(this.wordList)
-					this.count = this.wordList.length;
-					this.word = this.wordList[0].word;
+				let url = getUrl(FLASHCARD_URL+"/"+this.$store.state.vuex_user.id + "/"+date)
+				axios.get(url).then((res) => {
+					console.log(res.data)
+					if (res.data) {
+						let obj = res.data
+						console.log(obj[Object.keys(obj)[0]])
+						this.wordList = getArray(obj[Object.keys(obj)[0]].word_list)
+						console.log(this.wordList)
+						this.count = this.wordList.length;
+						this.word = this.wordList[0].word;
+					} else {
+						this.ShowCard = false;
+					}
 				})				
 			}else{
 				this.ShowCard = false;
@@ -123,8 +144,16 @@
 					this.noTask = false;
 				}
 			},
+			audio(src) {
+			  const iac = uni.createInnerAudioContext();
+			  iac.src = src;
+			  iac.play(() => {
+			    console.log("play~");
+			  });
+			},
 			rememberEvent:function(word, index){
 				// this.word = ''
+				let audio =  "../../static/sounds/notification_simple-02.wav"
 				if(index<this.wordList.length-1){
 					this.word = this.wordList[index+1].word
 				}
@@ -137,10 +166,12 @@
 						
 					}
 				}
+				this.audio(audio)
 				// this.checkFinish(index)
 			},
 			forgetEvent:function(word, index){
 				// this.word = ''
+				let audio = "../../static/sounds/navigation_transition-left.wav"
 				if(index<this.wordList.length-1){
 					this.word = this.wordList[index+1].word
 				}
@@ -153,9 +184,12 @@
 				}
 				this.forgetWords.push(word)
 				console.log(word)
+				this.audio(audio)
 				// this.checkFinish(index)
 			},
 			async reloadForget(){
+				let audio =  "../../static/sounds/hero_decorative-celebration-02.wav"
+				this.audio(audio)
 				this.$u.vuex('flash_words', this.wordList)
 				this.$u.route("/pages/flashcard/Summary");
 				// this.wordList = this.forgetWords;
@@ -223,6 +257,9 @@ uni-page-body {
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
+}
+.click-btn {
+	text-align: center;
 }
 .no-data {
       background-color: white;
