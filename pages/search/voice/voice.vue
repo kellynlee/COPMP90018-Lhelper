4fed9b3f9c8df609995a6be72180813c710aa377
Rex Ding <dongchengd1@student.unimelb.edu.au>
2021-11-04 13:11:20 +0800

    update voice

--- pages/search/voice/voice.vue
+++ pages/search/voice/voice.vue
@@ -1,63 +1,114 @@
 <template>
-	<view class="voice-page">
-		<sound-recording></sound-recording>
-		<view class="search-area">
-				<u-field
-					v-model="word"
-					label="Word:"
-				>
-				</u-field>
+	<view class="photo-page">
+		<view class="photo-area">
+				
+			<button class="image-selection uni-button" @longpress="startRecognize" @touchend="stopRecognize">{{btnText}}</button>
+				
+		</view>
+		<transition name="fade">
+			<view class="selection-area" v-if="textLst.length>0">
+				<text>Please select the word to see the dictionary</text>
+				<view v-for="(item,index) in textLst" :key="index" class="select-item">
+					<u-button class="button uni-button" >{{item}}</u-button>
+				</view>
 			</view>
-		<!-- <view>{{a}}</view> -->
-		<u-button  @click="searchIndex">submit</u-button>
-		<view>{{wordLst}}</view>
+		</transition>
 	</view>
-	
-	
 </template>
 
-<script>
-	import soundRecording from '@/components/sound-recording/sound-recording.vue';
-	let word_json = require('@/pages/search/words.json');
-	export default {
-		components: {soundRecording},
-		data() {
-			return {
-				a:word_json,
-				word:"",
-				wordLst:["abc"],
+<style lang="scss" scoped>
+	.photo-page {
+		background-color: white;
+		margin: 1rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+		border-radius: 1rem;
+		.photo-area {
+			display: flex;
+			flex-direction: column;
+			margin: 1rem 0;
+			.bor{
+				border:1rpx dashed #78827f;
+				}
+			.image-selection{
+				margin-top: 1rem;
+				background-color: #90caf9;
+				color: white;
 			}
-		},
-		methods: {
-			searchIndex(){
-				this.wordLst=[]
-				var len = word_json.words.length;
-				var firstP = false;
-				for (var i = 0; i < len; i++) { 
-				    var testWord = word_json.words[i]
-					if (testWord.indexOf(this.word) == 0){
-						this.wordLst.push(testWord);
-					}else{
-						if (firstP){
-							break;
-						}
-					}
-					
-				 }
+			.uni-button:after {
+			  border: none;
+			}
+		}
+		.selection-area {
+			margin: 0 1rem 1rem 1rem;
+			display: flex;
+			flex-direction: column;
+			.select-item {
+				margin: 0.5rem 0;
+				.button {
+					background-color: #c8e6c9;
+					color: white;
+				}
+				.uni-button:after {
+				  border: none;
+				}
 			}
 		}
 	}
+	.fade-enter-active, .fade-leave-active {
+	  transition: opacity .5s;
+	}
+	.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+	  opacity: 0;
+	}
+	
+	.image {
+		margin: 28rpx;
+	}
+	
+</style>
+
+<script>
+	import soundRecording from '@/components/sound-recording/sound-recording.vue';
+		
+	const recorderManager = uni.getRecorderManager();
+	const innerAudioContext = uni.createInnerAudioContext();
+	innerAudioContext.autoplay = true;
+	var _this;
+	
+	export default {
+		data() {
+			return {
+				textLst:[],
+				btnText:"Long Press To Recognize",
+			}
+		},
+			
+		methods: {
+			startRecognize() {
+							let _this = this;
+							let options = {};
+							_this.wordLst = [];
+							options.engine = _this.speechEngine;
+							options.punctuation = false;
+							options.lang = "en-us";
+							options.userInterface = false;
+							_this.searchText = ""
+			
+							plus.speech.startRecognize(options, function(s) {
+								_this.searchText = _this.searchText + s;
+								console.log("result: "+_this.searchText);
+								_this.textLst.push(s)
+							});
+						},
+			stopRecognize() {
+							setTimeout(function(){plus.speech.stopRecognize();}, 5000)
+						},
+			
+		}
+	};
+
 </script>
 
-<style lang="scss" scoped>
-.voice-page {
-	margin: 1rem;
-	background-color: white;
-	border-radius: 1rem;
-	box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
-	  rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
-	.selection-area {
-		
-	}
-}
-</style>
