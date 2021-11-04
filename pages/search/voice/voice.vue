7507b7c5e42ae4bb02be6c0dbd62374245bf2f86
Rex Ding <dongchengd1@student.unimelb.edu.au>
2021-11-04 20:32:30 +0800

    Revert "combine image with voice"
    This reverts commit fc9598d8579f9f4fc3d70056efe5d98e5bba520e.

--- pages/search/voice/voice.vue
+++ pages/search/voice/voice.vue
@@ -1,103 +1,114 @@
 <template>
-	<view>
-
-		
-		<uni-fab ref="fab" :pattern="pattern" :content="content" :horizontal="horizontal" :vertical="vertical" :direction="direction"
-		 @trigger="trigger" @fabClick="fabClick" />
-		 
-		<u-popup v-model="showPopUp" mode="center" border-radius="14">
-		 		<h3 class="popup-title">
-		 			Voice Recording
-		 		</h3>
-				<button class="voice-button" @longpress="startRecognize" @touchend="stopRecognize">Long Press To Record</button>
-		</u-popup>
-		
-	
-		
+	<view class="photo-page">
+		<view class="photo-area">
+				
+			<button class="image-selection uni-button" @longpress="startRecognize" @touchend="stopRecognize">{{btnText}}</button>
+				
+		</view>
+		<transition name="fade">
+			<view class="selection-area" v-if="textLst.length>0">
+				<text>Please select the word to see the dictionary</text>
+				<view v-for="(item,index) in textLst" :key="index" class="select-item">
+					<u-button class="button uni-button" >{{item}}</u-button>
+				</view>
+			</view>
+		</transition>
 	</view>
 </template>
-<style>
-	.popup-title{
-		text-align:center;
-		margin-top: 30rpx;
-	},
-	.voice-button {
-		margin: 20rpx
+
+<style lang="scss" scoped>
+	.photo-page {
+		background-color: white;
+		margin: 1rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+		border-radius: 1rem;
+		.photo-area {
+			display: flex;
+			flex-direction: column;
+			margin: 1rem 0;
+			.bor{
+				border:1rpx dashed #78827f;
+				}
+			.image-selection{
+				margin-top: 1rem;
+				background-color: #90caf9;
+				color: white;
+			}
+			.uni-button:after {
+			  border: none;
+			}
+		}
+		.selection-area {
+			margin: 0 1rem 1rem 1rem;
+			display: flex;
+			flex-direction: column;
+			.select-item {
+				margin: 0.5rem 0;
+				.button {
+					background-color: #c8e6c9;
+					color: white;
+				}
+				.uni-button:after {
+				  border: none;
+				}
+			}
+		}
 	}
-</style>
-<script>
+	.fade-enter-active, .fade-leave-active {
+	  transition: opacity .5s;
+	}
+	.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+	  opacity: 0;
+	}
 	
+	.image {
+		margin: 28rpx;
+	}
+	
+</style>
+
+<script>
+	import soundRecording from '@/components/sound-recording/sound-recording.vue';
+		
 	const recorderManager = uni.getRecorderManager();
 	const innerAudioContext = uni.createInnerAudioContext();
 	innerAudioContext.autoplay = true;
 	var _this;
 	
 	export default {
-		
-		data(){
+		data() {
 			return {
-				showPopUp: false,
-				wordLst:[],
-				horizontal: 'right',
-				vertical: 'bottom',
-				direction: 'horizontal',
-				pattern: {
-					color: '#7A7E83',
-					backgroundColor: '#fff',
-					selectedColor: '#007AFF',
-					buttonColor: '#c7e5c8'
-				},
-				content: [{
-						iconPath: '/static/icons/photo.png',
-						selectedIconPath: '/static/icons/photo.png',
-						text: 'Photo',
-						active: false
-					},
-					{
-						iconPath: '/static/sound-recording/voice.png',
-						text: 'Voice',
-						active: false
-					},
-				]
+				textLst:[],
+				btnText:"Long Press To Recognize",
 			}
 		},
-
+			
 		methods: {
-			
-			trigger(e) {
-				console.log(e)
-				this.showPopUp = true
-				// this.content[e.index].active = !e.item.active
-				
-			},
-			fabClick() {
-				uni.showToast({
-					title: '点击了悬浮按钮',
-					icon: 'none'
-				})
-			},
-			
 			startRecognize() {
-				let _this = this;
-				let options = {};
-				_this.wordLst = [];
-				options.engine = _this.speechEngine;
-				options.punctuation = false;
-				options.lang = "en-us";
-				options.userInterface = false;
-				_this.searchText = ""
-
-				plus.speech.startRecognize(options, function(s) {
-					_this.searchText = _this.searchText + s;
-					console.log("result: "+_this.searchText);
-					_this.wordLst.push(s)
-				}, function(e){
-					console.log(e.message);
-				});
-			},
+							let _this = this;
+							let options = {};
+							_this.wordLst = [];
+							options.engine = _this.speechEngine;
+							options.punctuation = false;
+							options.lang = "en-us";
+							options.userInterface = false;
+							_this.searchText = ""
+			
+							plus.speech.startRecognize(options, function(s) {
+								_this.searchText = _this.searchText + s;
+								console.log("result: "+_this.searchText);
+								_this.textLst.push(s)
+							});
+						},
 			stopRecognize() {
-				setTimeout(function(){plus.speech.stopRecognize();}, 2000)
-			},
+							setTimeout(function(){plus.speech.stopRecognize();}, 5000)
+						},
+			
 		}
-	}   
+	};
+
 </script>
+
